b787482540a9cdafab6e32675cb05d122bd2577e
test <test@9750-5007-6974>
2024-04-03 14:01:37 +0200

    update path

--- registration.php
+++ registration.php
@@ -2,5 +2,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'TwentyToo_CustomTab',
-    __DIR__
+    __DIR__ . '/CustomTab'
 );
